ccede17d25c6d2f53bec3f7d9e2f0ebf51ab8909
Jagger <634750802@qq.com>
2024-12-11 16:10:42 +0800

    update

--- start-autoflow.sh
+++ start-autoflow.sh
@@ -4,6 +4,8 @@ set -e
 
 echo $AUTOFLOW_VERSION
 
+docker compose pull --policy always
+
 docker compose run --rm backend /bin/sh -c "alembic upgrade head"
 docker compose run --rm backend /bin/sh -c "python bootstrap.py" >> bootstrap.stdout
 
--- start-tidb.sh
+++ start-tidb.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-tiup playground v8.4.0 --tag ${PLAYGROUND_TAG:-"autoflow-demo"} --db 1 --pd 1 --tiflash 1 --kv 1
+tiup playground v8.4.0 --tag ${PLAYGROUND_TAG:-"autoflow-demo"} --db 1 --pd 1 --tiflash 1 --kv 1 --without-monitor
+
+tiup clean autoflow-demo
